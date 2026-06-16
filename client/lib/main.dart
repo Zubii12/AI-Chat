ff5020cdf423dcb5d64f0191d9dad971b5f7f7d7
Zubii <ionutzubascu@yahoo.com>
2026-06-16 17:09:41 +0300

    feat: initialize Supabase with .env file

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:ai_chat/src/bootstrap.dart';
 import 'package:ai_chat/src/ui/app/app.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
+  );
+
   await bootstrap(() => const App());
 }
